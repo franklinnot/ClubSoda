dce4f7768c695ec5614fc5a92026d39137379b30
franklinnot <franklinnot@outlook.com>
2025-06-15 14:40:55 -0500

    fix(routes): remove duplicate entry for "Nosotros" route

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,10 +1,10 @@
 
 const routes = [
   { title: "Inicio", route: "/" },
+  { title: "Nosotros", route: "/us" },
   { title: "Productos", route: "/products/catalog" },
   { title: "Compras", route: "/purchase/history" },
   { title: "Consultas", route: "/request/history" },
-  { title: "Nosotros", route: "/us" }
 ];
 
 export default function getRoutes(){
