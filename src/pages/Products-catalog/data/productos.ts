43397dbe6bff40414f5a53f91eed4d81e4ef90d2
Belen <anabelensanchezboy@gmail.com>
2025-06-15 23:20:17 -0500

    Ingreso de Productos

--- src/pages/Products-catalog/data/productos.ts
+++ src/pages/Products-catalog/data/productos.ts
@@ -33,4 +33,77 @@ export const productos = [
         descripcion: "Caja de chiki wafer fiesta 8 bolsones x caja de sabores surtidos (vainilla, chocolate, fresa y limón) "
 
     },
+    {
+        id:"1100037",
+        nombre: "Mermelada de fresa San Jorge pote & Soda San Jorge",
+        precio: 5.30,
+        categoria: ["Mermeladas", "Galletas", "Venta por Unidad"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_046b35888ea29360330338089a5002188572749a_producto_large_90.png?not-from-cache-please",
+        descripcion: "Mermelada de fresa San Jorge pote 320 g - Pqt. Soda x 40 g"
+    },
+    {
+        id:"1100051",
+        nombre: "Pack La Loncherita",
+        precio: 60.20,
+        categoria: ["Galletas", "Venta por Caja"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_aac8c58f603614d1439b14808c098a67c9b283e3_producto_large_90.webp",
+        descripcion: "1 Caja de Rellenitas Taco sabor Fresa x 90g - 1 Caja de Soda San Jorge 6+1 28 und de 40gr c/u - 1 Caja de Mega Wafer sabor vainilla x 61gr c/u "
+    },
+    {
+        id:"0101007",
+        nombre: "Bolsón de Galletas Animalitos",
+        precio: 13.20,
+        categoria: ["Galletas", "Venta por Bolsón"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_b2f6971eb95f15ca46c694859dd5a646407cadae_producto_large_90.jpg",
+        descripcion: "CANTIDAD: 10 UNIDADES POR BOLSÓN"
+    },
+    {
+        id:"0101020",
+        nombre: "Animalitos X 80 Bls. X 60 gr.",
+        precio: 50.30,
+        categoria: ["Galletas", "Venta por Caja"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_1bc1821e8a67683a26d6e1d91f5550a578a97e80_producto_large_90.jpg",
+        descripcion: "4 Bolsones de 20 unidades de 60g por Caja"
+    },
+    {
+        id:"0101900",
+        nombre: "Animalitos San Jorge x 500g",
+        precio: 4.50,
+        categoria: ["Galletas", "Venta por Unidad"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_c7ac1cf64da5983b879ec0cbd871f5e3d3f67550_producto_medium_80.png",
+        descripcion: "Galletas Animalitos bolsa x 500g San Jorge"
+    },
+    {
+        id:"0102903",
+        nombre: "Soda en Bandeja San Jorge",
+        precio: 5.30,
+        categoria: ["Galletas", "Venta por Unidad"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_bad63eef778f83063e15a8247c9018f6855ad2fa_producto_large_90.jpg",
+        descripcion: "Soda en Bandeja x 500 gr - San Jorge"
+    },
+    {
+        id:"0101912",
+        nombre: "Mini Rondelas x 55g San Jorge",
+        precio: 0.90,
+        categoria: ["Galletas", "Venta por Unidad"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_d92f143b81e0684df3ee83eca06cb5935d498f45_producto_large_90.png",
+        descripcion: "Galletas Mini Rondelas bolsa x 55g San Jorge"
+    },
+    //
+    {
+        id:"0101007",
+        nombre: "Bolsón de Galletas Animalitos",
+        precio: 13.20,
+        categoria: ["Galletas", "Venta por Bolsón"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_b2f6971eb95f15ca46c694859dd5a646407cadae_producto_large_90.jpg",
+        descripcion: "CANTIDAD: 10 UNIDADES POR BOLSÓN"
+    },
+    {
+        id:"0101007",
+        nombre: "Bolsón de Galletas Animalitos",
+        precio: 13.20,
+        categoria: ["Galletas", "Venta por Bolsón"],
+        imagen: "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010715/tienda_010715_b2f6971eb95f15ca46c694859dd5a646407cadae_producto_large_90.jpg",
+        descripcion: "CANTIDAD: 10 UNIDADES POR BOLSÓN"
+    },
 ];
